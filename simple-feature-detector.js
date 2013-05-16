b84c2a6020ad6a03c80551f08a9aa30a01a3f43d
Adam Lynch <contact@adamlynch.ie>
2013-05-17 00:01:55 +0200

    fixed identation w/ PHPStorm auto-format

--- simple-feature-detector.js
+++ simple-feature-detector.js
@@ -4,46 +4,46 @@
  *
  * @author: adam-lynch
  */
-define(function(){
+define( function(){
 	return new function(){
 		var _featureDetector = this,
 
 			/**
-		 * Takes a string container an initial for each vendor would be acceptable and returns a list of their vendor prefixes
-		 *
-		 * See this.vendorPrefixes for possible values. "all" is also supported
-		 *
-		 * @param prefixes | a string; e.g. "wgt" which means Webkit, Gecko (Moz) or a Trident (MS) prefixed property is ok
-		 * @returns {Array}
-		 */
-			_buildListOfAcceptableVendorPrefixes = function( prefixes ){
-			var vendors = [];
+			 * Takes a string container an initial for each vendor would be acceptable and returns a list of their vendor prefixes
+			 *
+			 * See this.vendorPrefixes for possible values. "all" is also supported
+			 *
+			 * @param prefixes | a string; e.g. "wgt" which means Webkit, Gecko (Moz) or a Trident (MS) prefixed property is ok
+			 * @returns {Array}
+			 */
+				_buildListOfAcceptableVendorPrefixes = function( prefixes ){
+				var vendors = [];
 
-			for( var c = 0, length = ('all' === prefixes ? 'gkpw' : prefixes).length; c < length; c++ ){
-				vendors.push( '-' + _vendorPrefixes[prefixes[c]] + '-' );
-			}
+				for( var c = 0, length = ('all' === prefixes ? 'gkpw' : prefixes).length; c < length; c++ ){
+					vendors.push( '-' + _vendorPrefixes[prefixes[c]] + '-' );
+				}
 
-			return vendors;
-		},
+				return vendors;
+			},
 
 			_propertyExists = function( property ){
-			return 'undefined' !== _testElement.style[property];
+				return 'undefined' !== _testElement.style[property];
 			},
 
 			_testElement = document.createElement( 'div' ),
 
 			_vendorPrefixes = {
-			g: 'moz', //Gecko
-			k: 'k', //KHTML (Konqueror)
-			p: 'o', //Presto (Opera)
-			t: 'ms', //Trident (IE)
-			w: 'webkit'
+				g: 'moz', //Gecko
+				k: 'k', //KHTML (Konqueror)
+				p: 'o', //Presto (Opera)
+				t: 'ms', //Trident (IE)
+				w: 'webkit'
 			},
 
-			_convertHyphenatedToCamelCase = function(str){
-			return str.replace( /-([a-z])/g, function( matches ){
-				return matches[1].toUpperCase();
-			});
+			_convertHyphenatedToCamelCase = function( str ){
+				return str.replace( /-([a-z])/g, function( matches ){
+					return matches[1].toUpperCase();
+				} );
 			};
 
 		/**
@@ -133,4 +133,4 @@ define(function(){
 			return false;
 		};
 	};
-});
+} );
