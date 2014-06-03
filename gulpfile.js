683248b96ac9c427a47e0bad1d3a20c4bb4cf418
Adam Lynch <contact@adamlynch.ie>
2014-06-03 22:16:47 +0100

    Forgot to commit some stuff

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,8 @@ gulp.task('minify', function(done){
        .pipe(gulp.dest('./'))
        .on('end', done);
 });
+
+gulp.task('bump', function(done){
+    gulp.src('./simple-feature-detector.js')
+        .pipe($.replace(/(simple-feature-detector v)[0-9]+\.[0-9]+\.[0-9]+( - )/, '$1' + newVersion + '$2'))
+});
