4bd51342944105a4e6f0ef36778426210d22f998
Adam Lynch <contact@adamlynch.ie>
2014-06-10 23:17:13 +0100

    Progress with PHP-independent testing

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,25 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var semver = require('semver');
 var inquirer = require('inquirer');
+var combine = require('stream-combiner');
 
-var mainFile = './simple-feature-detector.js';
+var paths = {};
+paths.mainFile = './simple-feature-detector.js';
+paths.testRoot = './test/';
+paths.testConfigRoot = paths.testRoot + 'config/';
+paths.jasmineRoot = paths.testConfigRoot + 'jasmine/';
+paths.testUtilsRoot = paths.testRoot + 'util/';
+paths.testSuitesRoot = paths.testRoot + 'suites/';
+
+var compile = combine(
+    $.uglify({
+        preserveComments: 'some'
+    })
+    .pipe($.rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest('./'))
+);
 
 gulp.task('default', ['bump']);
 
@@ -22,16 +39,10 @@ gulp.task('bump', function(done){
         var oldVersion =  require('./package.json').version,
             newVersion = semver.inc(oldVersion, result.bump);
 
-        gulp.src(mainFile)
+        gulp.src(paths.mainFile)
             .pipe($.replace(new RegExp('(simple-feature-detector v)' + oldVersion.replace('.', '\\.')), '$1' + newVersion))
             .pipe(gulp.dest('./'))
-            .pipe($.uglify({
-                preserveComments: 'some'
-            }))
-            .pipe($.rename({
-                suffix: '.min'
-            }))
-            .pipe(gulp.dest('./'));
+            .pipe(compile);
 
         gulp.src('./*.json')
             .pipe($.bump({
@@ -41,4 +52,46 @@ gulp.task('bump', function(done){
 
         setTimeout(done, 200);
     });
+});
+
+gulp.task('test', ['compile', 'compile-test'], function(done){
+    gulp.src(paths.testRoot + 'index.html')
+        .pipe($.exec([
+                'phantomjs',
+                paths.testConfigRoot + 'phantomjs_jasminexml_runner.js',
+                paths.testRoot + 'test-runner.html',
+                paths.testRoot + 'results'
+        ].join(' ')))
+        .on('end', done);
+});
+
+gulp.task('view-tests', ['compile', 'compile-test'], function(done){
+    gulp.src(paths.testRoot + 'index.html')
+        .pipe($.open())
+        .on('end', done);
+});
+
+gulp.task('compile-test', function(done){
+    gulp.src([
+        paths.jasmineRoot + 'jasmine.js',
+        paths.jasmineRoot + 'jasmine-html.js',
+        paths.jasmineRoot + 'jasmine.phantomjs-reporter.js',
+        paths.testUtilsRoot + '*.js',
+        './simple-feature-detector.min.js',
+        paths.testSuitesRoot + '*.js',
+    ])
+        .pipe($.concat('script.js'))
+        .pipe($.insert.append('\n' + [
+            'jasmine.getEnv().addReporter(new jasmine.TrivialReporter());',
+            'if(jasmine.PhantomJSReporter) jasmine.getEnv().addReporter(new jasmine.PhantomJSReporter());',
+            'jasmine.getEnv().execute();'
+        ].join('\n')))
+        .pipe(gulp.dest(paths.testRoot))
+        .on('end', done);
+});
+
+gulp.task('compile', function(done){
+    gulp.src(paths.mainFile)
+        .pipe(compile)
+        .on('end', done);
 });
